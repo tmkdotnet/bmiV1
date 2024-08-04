1255345d9b68eb8eb6d68f4c7a6b08eebf4f87ce
tmkdotnet <tomaszrys@tmknet.pl>
2024-08-04 13:21:37 +0200

    bmi app v1

---
+++ bmi1/bmi1/Bmi.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace bmi1.Model
+{
+    internal class Bmi
+    {
+        private double _weight;
+        private double _height;
+
+        public double Weight
+        {
+            get => _weight;
+            set
+            {
+                if (value > 0)
+                {
+                    _weight = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Waga musi byc +");
+                }
+
+            }
+
+        }
+
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if (value > 0)
+                {
+                    _height = value;
+                }
+                else
+                {
+
+                    throw new ArgumentException("Wzrost musi byc +");
+                }
+            }
+
+        }
+
+
+        public Bmi(double weight, double height)
+        {
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException($"Waga musi byc +");
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentException($"Wzrost musi byc +");
+            }
+
+            this._weight = weight;
+            this._height = height;
+        }
+
+
+        public double CalculateBmi()
+        {
+
+            if (_height == 0)
+            {
+                throw new InvalidOperationException("Wzrost musi byc wiekszy niz  0");
+            };
+            // BMI = waga (kg) / (wzrost (m) * wzrost (m))
+
+            double heightInMeters = _height / 100; // Zakładając, że wzrost jest w centymetrach
+            double bmi = _weight / (heightInMeters * heightInMeters);
+            return Math.Round(bmi,1);
+        }
+    }//End BMI
+}
---
+++ bmi1/bmi1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using bmi1.Model;
+
+namespace bmi1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ClickBmi(object sender, RoutedEventArgs e)
+        {
+            double weightFromUI;
+            double heigthFromUI;
+
+            bool weightIsValid = double.TryParse(txt1H.Text, out weightFromUI);
+            bool heigthIsValid = double.TryParse(txt2W.Text, out heigthFromUI);
+
+            if (weightIsValid && heigthIsValid)
+            {
+                try
+                {
+                 
+                    Bmi bmi = new Bmi( weightFromUI, heigthFromUI );
+
+                    double yourBmi =  bmi.CalculateBmi();
+                    txtB1.Text = yourBmi.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd: {ex.Message}");
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Wprowadź prawidłowe wartości liczby.");
+            }
+        }
+    }
+}
